6e241a68bdd65daba8483261b1bb0295b6ccc2cf
BennyZ3 <bennyz3@hotmail.com>
2021-12-17 17:08:35 -0500

    first pet and num of pets

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
-    return (
-      <section className="first-pet">
-        <h4>Is this your first pet?</h4>
-        <br />
-        <form>
-          <input type="checkbox" />
-        </form>
-        <h5></h5>
-      </section>
-    );
-  }
-}
+const FirstPet = () => {
+  const [checked, setChecked] = useState(false);
+  const handleCheckBox = (e) => {
+    setChecked(!checked);
+  };
+  return (
+    <section className="first-pet">
+      <h4>Is this your first pet?</h4>
+      <br />
+      <form>
+        <input type="checkbox" onChange={handleCheckBox} />
+      </form>
+      <h5>{checked ? "yes" : "no"}</h5>
+    </section>
+  );
+};
 
 export default FirstPet;
--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
-  }
-}
+const NumberOfPets = () => {
+  const [numOfPets, setNumOfPets] = useState(0);
+
+  const handleMinus = () => {
+    if (numOfPets > 0) {
+      setNumOfPets(numOfPets - 1);
+    }
+  };
+
+  const handlePlus = () => {
+    setNumOfPets(numOfPets + 1);
+  };
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button id="less" onClick={handleMinus}>
+          -
+        </button>
+        <button id="more" onClick={handlePlus}>
+          +
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default NumberOfPets;
