bf1ece311f32dfdfacec56a3892e1cba26a64f32
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-17 17:48:03 -0500

    import useState, add useState with count and setCount, add eventlistener handleCounter on both buttons

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NumberOfPets.css";
 
 const NumberOfPets = () => {
+
+    let [count, setCount] = useState(0)
+
+    const handleCounter = (event) => {
+      setCount(count = event.target.id === "less" ? count - 1 : count + 1)
+    }
+
     return (
       <section className="num-of-pets">
         <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
+        <h5>{count}</h5>
         <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
+          <button onClick={handleCounter} id="less">-</button>
+          <button onClick={handleCounter} id="more">+</button>
         </div>
       </section>
     );
