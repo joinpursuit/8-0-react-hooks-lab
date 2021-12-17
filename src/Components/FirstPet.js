7e22339e97039ba21e1ee3bc4465de384b5a6496
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-17 17:27:43 -0500

    convert FirstPet class component to functional component, add useState, add handleChange, add condition to show yes or no in h5

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
+const FirstPet = () => {
+  const [isFirstPet, setFirstPet] = useState(false)
+
+  const handleChange = () => {
+    setFirstPet(!isFirstPet)
+  }
+
     return (
       <section className="first-pet">
         <h4>Is this your first pet?</h4>
         <br />
         <form>
-          <input type="checkbox" />
+          <input onChange={handleChange} type="checkbox" />
         </form>
-        <h5></h5>
+        <h5>{isFirstPet ? "Yes" : "No"}</h5>
       </section>
     );
-  }
 }
 
 export default FirstPet;
