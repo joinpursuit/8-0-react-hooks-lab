8e9d9035629ed4d79e4ec1b1033553b7b93f175a
Jalamang <Jalamangjanka@gmail.com>
2021-12-17 18:51:53 -0500

    added animalTypes in state and functions to remove pet and add pet to existing list.

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import "./AnimalTypes.css";
 
-const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
-class AnimalTypes extends React.Component {
-  render() {
-    return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol></ol>
-      </section>
-    );
+
+const AnimalTypes = () => {
+
+  const [useObj, setObj] = useState({
+    userInput: '',
+    animalTypes: ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"]
+  })
+
+  const handleInputChange = (event) => {
+    setObj({...useObj, userInput: event.target.value})
   }
+
+  const handleRemoveAnimal = (event) => {
+    setObj({...useObj, animalTypes: useObj.animalTypes.filter(el => el !== useObj.animalTypes[event.target.id])})
+  }
+
+  const handleAddPet = (event) => {
+    event.preventDefault()
+    setObj({...useObj, animalTypes: [...useObj.animalTypes, useObj.userInput]})
+  }
+
+  return (
+    <section className={"animal-types"}>
+      <h4>Animal Types</h4>
+      <form onSubmit={handleAddPet}>
+        <label htmlFor="type">
+          <input onChange={handleInputChange} type="text" id="animal-type" />
+        </label>
+        <input type="submit" />
+      </form>
+      <ol>{useObj.animalTypes.map((el, index) =>  <li>{el}<button id={index} onClick={handleRemoveAnimal}></button></li>)}</ol>
+    </section>
+  );
 }
 
 export default AnimalTypes;
