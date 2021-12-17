4ac2aba3e436333d31b4eff27901bb9bfcb4e663
BennyZ3 <bennyz3@hotmail.com>
2021-12-17 18:04:33 -0500

    finished new client info

--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -1,32 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewClient.css";
 
-class NewClient extends React.Component {
-  render() {
-    return (
-      <section className="new-client">
-        <h4>New Client Info</h4>
-        <div>
-          <form>
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" />
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" />
-            <label htmlFor="phone">Telephone</label>
-            <input type="tel" id="phone" />
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
-          </form>
-          <article>
-            <h5>firstName</h5>
-            <h5>lastName</h5>
-            <h5>phone</h5>
-            <h5>email</h5>
-          </article>
-        </div>
-      </section>
-    );
-  }
-}
+const NewClient = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    console.log(e.target.value);
+    switch (e.target.id) {
+      case "firstName":
+        setFirstName(e.target.value);
+        break;
+      case "lastName":
+        setLastName(e.target.value);
+        break;
+      case "phone":
+        setPhone(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+    }
+  };
+
+  return (
+    <section className="new-client">
+      <h4>New Client Info</h4>
+      <div>
+        <form onChange={(e) => handleSubmit(e)}>
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" />
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" />
+          <label htmlFor="phone">Telephone</label>
+          <input type="tel" id="phone" />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </form>
+        <article>
+          <h5>{firstName}</h5>
+          <h5>{lastName}</h5>
+          <h5>{phone}</h5>
+          <h5>{email}</h5>
+        </article>
+      </div>
+    </section>
+  );
+};
 
 export default NewClient;
