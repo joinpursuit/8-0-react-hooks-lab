87bb749255fe960009b7ce6ba2ac546f09b75049
salrey <tekdehn@gmail.com>
2021-12-17 17:19:27 -0500

    npm install, setup index, update index, navbar, footer

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,17 @@
 import React from "react";
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
-    return (
-      <section className="first-pet">
-        <h4>Is this your first pet?</h4>
-        <br />
-        <form>
-          <input type="checkbox" />
-        </form>
-        <h5></h5>
-      </section>
-    );
-  }
-}
+const FirstPet = () => {
+  return (
+    <section className="first-pet">
+      <h4>Is this your first pet?</h4>
+      <br />
+      <form>
+        <input type="checkbox" />
+      </form>
+      <h5></h5>
+    </section>
+  );
+};
 
 export default FirstPet;
