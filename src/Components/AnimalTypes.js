f30783028d2d42a6b928221df638ced63884ee09
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-17 18:52:41 -0500

    add useState with useObj and setObj, add handleInputChange, handleRemoveAnimal and handleAddPet functions, add onSubmit eventlistener to form, add onChange eventlisterner to input, add .map in ol

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,19 +1,38 @@
 import React, {useState} from "react";
 import "./AnimalTypes.css";
 
-const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
+
 
 const AnimalTypes = () => {
+
+  const [useObj, setObj] = useState({
+    userInput: '',
+    animalTypes: ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"]
+  })
+
+  const handleInputChange = (event) => {
+    setObj({...useObj, userInput: event.target.value})
+  }
+
+  const handleRemoveAnimal = (event) => {
+    setObj({...useObj, animalTypes: useObj.animalTypes.filter(el => el !== useObj.animalTypes[event.target.id])})
+  }
+
+  const handleAddPet = (event) => {
+    event.preventDefault()
+    setObj({...useObj, animalTypes: [...useObj.animalTypes, useObj.userInput]})
+  }
+
   return (
     <section className={"animal-types"}>
       <h4>Animal Types</h4>
-      <form>
+      <form onSubmit={handleAddPet}>
         <label htmlFor="type">
-          <input type="text" id="animal-type" />
+          <input onChange={handleInputChange} type="text" id="animal-type" />
         </label>
         <input type="submit" />
       </form>
-      <ol></ol>
+      <ol>{useObj.animalTypes.map((el, index) =>  <li>{el}<button id={index} onClick={handleRemoveAnimal}></button></li>)}</ol>
     </section>
   );
 }
