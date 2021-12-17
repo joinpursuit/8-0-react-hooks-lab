98f23d3d72e9a0c0fc26880f61d48fce46235a4c
Jalamang <Jalamangjanka@gmail.com>
2021-12-17 18:17:35 -0500

    adding new client

--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -2,7 +2,17 @@ import React, { useState} from "react";
 import "./NewClient.css";
 
 const NewClient = () =>{
-const [] = useState()
+const [formInfo, setFormInfo] = useState({
+  firstName:"",
+  lastName:"",
+  phone:"",
+  email:""
+})
+
+const handleChange = (event) =>{
+const {value, id} = event.target
+setFormInfo({...formInfo, [id]:value})
+}
  
   
     return (
@@ -11,19 +21,19 @@ const [] = useState()
         <div>
           <form>
             <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" />
+            <input onChange={handleChange} type="text" id="firstName" />
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" />
+            <input onChange={handleChange} type="text" id="lastName" />
             <label htmlFor="phone">Telephone</label>
-            <input type="tel" id="phone" />
+            <input onChange={handleChange} type="tel" id="phone" />
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input onChange={handleChange} type="email" id="email" />
           </form>
           <article>
-            <h5>firstName</h5>
-            <h5>lastName</h5>
-            <h5>phone</h5>
-            <h5>email</h5>
+            <h5>{formInfo.firstName}</h5>
+            <h5>{formInfo.lastName}</h5>
+            <h5>{formInfo.phone}</h5>
+            <h5>{formInfo.email}</h5>
           </article>
         </div>
       </section>
