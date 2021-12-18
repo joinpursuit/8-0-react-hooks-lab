ca7501c048ebeb221138d2a2c31a415c66d3897b
salrey <tekdehn@gmail.com>
2021-12-17 19:43:47 -0500

    Update NewClient

--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewClient.css";
 
-class NewClient extends React.Component {
-  render() {
-    return (
-      <section className="new-client">
-        <h4>New Client Info</h4>
-        <div>
-          <form>
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" />
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" />
-            <label htmlFor="phone">Telephone</label>
-            <input type="tel" id="phone" />
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
-          </form>
-          <article>
-            <h5>firstName</h5>
-            <h5>lastName</h5>
-            <h5>phone</h5>
-            <h5>email</h5>
-          </article>
-        </div>
-      </section>
-    );
+const NewClient = () => {
+
+  const [ handleForm, setHandleForm ] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: ""
+  })
+
+  const handleInputs = (event) => {
+    setHandleForm({...handleForm, [event.target.name]: event.target.value })
+    console.log(handleForm)
   }
-}
+
+  return (
+    <section className="new-client">
+      <h4>New Client Info</h4>
+      <div>
+        <form>
+          <label htmlFor="firstName">First Name</label>
+          <input onChange={handleInputs} type="text" id="firstName" name="firstName" />
+          <label htmlFor="lastName">Last Name</label>
+          <input onChange={handleInputs} type="text" name="lastName" id="lastName" />
+          <label htmlFor="phone">Telephone</label>
+          <input onChange={handleInputs} type="tel" id="phone" name="phone" />
+          <label htmlFor="email">Email</label>
+          <input onChange={handleInputs} type="email" id="email" name="email"/>
+        </form>
+        <article>
+          <h5>{handleForm.firstName}</h5>
+          <h5>{handleForm.lastName}</h5>
+          <h5>{handleForm.phone}</h5>
+          <h5>{handleForm.email}</h5>
+        </article>
+      </div>
+    </section>
+  );
+};
 
 export default NewClient;
