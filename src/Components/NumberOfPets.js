7cf2bbd57df874b65a08c6005e217acaaa49d0d8
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-12-18 15:10:52 -0500

    Hooks lab complete

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
+const NumberOfPets = () => {
+  let [numOfPets, setNumOfPets] = useState(0);
+
+  const handleAddPet = () => { setNumOfPets(++numOfPets) }
+
+  const handleRemovePet = () => { 
+    numOfPets = (numOfPets > 0 ? --numOfPets : 0);
+    setNumOfPets(numOfPets);
   }
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button id="less" onClick={handleRemovePet}>-</button>
+        <button id="more" onClick={handleAddPet}>+</button>
+      </div>
+    </section>
+  );
 }
 
 export default NumberOfPets;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
 const BASE_URL = "http://localhost:5000/api";
+const API = 'https://vet-api-8-1.herokuapp.com/api/pets';
 
-class PetList extends React.Component {
-  render() {
-    return (
-      <section className="pet-list">
-        <h4>All Pets</h4>
-        <article></article>
-      </section>
-    );
-  }
+const PetList = () => {
+  const [ petData, setPetData ] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+    .then((response) => response.json())
+    .then((data) => setPetData(data))
+    .catch((error) => { throw error })
+  },[])  
+
+  return (
+    <section className="pet-list">
+      <h4>All Pets</h4>
+      <article>
+        {petData.map((pet) => <Pet name={pet.name} breed={pet.breed} kind={pet.kind} /> )}
+      </article>
+    </section>
+  );
 }
 
 export default PetList;
