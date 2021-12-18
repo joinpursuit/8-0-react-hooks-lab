1fbc3d0b6981cf787c60783bb881fe491697ca16
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-17 19:32:58 -0500

    convert PetsList class component to functional component, import useState and useEffect

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://raw.githubusercontent.com/joinpursuit/resource-veterinarian-api/main/data/db.json";
 
-class PetList extends React.Component {
-  render() {
+const PetList = () => {
     return (
       <section className="pet-list">
         <h4>All Pets</h4>
         <article></article>
       </section>
     );
-  }
 }
 
 export default PetList;
