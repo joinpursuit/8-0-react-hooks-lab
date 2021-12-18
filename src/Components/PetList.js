72c04ebc04c10963e50a184784c028b4cca32db6
salrey <tekdehn@gmail.com>
2021-12-18 15:50:19 -0500

    Update PetList

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,14 +1,27 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "https://vet-api-8-1.herokuapp.com";
+const BASE_URL = "https://vet-api-8-1.herokuapp.com/api/pets";
 
 const PetList = () => {
+
+  const [ allPets, setAllPets ] = useState([])
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then((response) => response.json())
+    .then((pets) => setAllPets(pets))
+    .catch(console.log) 
+  },[])
+    // the empty array stops the function from running on update
+
+  const results = allPets.map(({id,name,breed, kind}) => <Pet key={id} name={name} breed={breed} kind={kind} />)
+
   return (
     <section className="pet-list">
       <h4>All Pets</h4>
-      <article></article>
+      <article>{results}</article>
     </section>
   );
 }
