982e2e0fd17cee6c77d1e6abbbcd1b4062874751
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-18 11:37:19 -0500

    NumberOfPets passes all tests with class component. No hooks.

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -2,14 +2,34 @@ import React from "react";
 import "./NumberOfPets.css";
 
 class NumberOfPets extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      count: 1,
+    }
+  }
+  
+  handleIncrement=()=>{
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  handleDecrement=()=>{
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
+  
   render() {
     return (
       <section className="num-of-pets">
         <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
+        <h5>{ this.state.count }</h5>
         <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
+          <button onClick={this.handleDecrement} id="less">-</button>
+          <button onClick={this.handleIncrement} id="more">+</button>
         </div>
       </section>
     );
