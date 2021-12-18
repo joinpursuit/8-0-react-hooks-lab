35c5eb7d8cbb116696a0c888dfa07e4c003efd53
salrey <tekdehn@gmail.com>
2021-12-17 19:09:57 -0500

    Update NumOfPets

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
+const NumberOfPets = () => {
+
+  const [ numOfPets, setNumOfPets ] = useState(0)
+
+  const handleDecrease = () => {
+    numOfPets > 0 && setNumOfPets(numOfPets-1)
   }
+
+  const handleIncrease = () => {
+    setNumOfPets(numOfPets+1)
+  }
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button onClick={handleDecrease}id="less">-</button>
+        <button onClick={handleIncrease} id="more">+</button>
+      </div>
+    </section>
+  );
 }
 
 export default NumberOfPets;
