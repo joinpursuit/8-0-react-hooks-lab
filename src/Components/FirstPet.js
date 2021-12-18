4956ab51bbb0e87431629e323f5e37021f34357d
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-18 11:16:26 -0500

    FirstPet.js passes with class component, no hooks

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -2,15 +2,33 @@ import React from "react";
 import "./FirstPet.css";
 
 class FirstPet extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      display: true,
+    }
+  }
+
+  handleCheckbox=(e)=>{
+    this.setState({
+      display: e.target.checked,
+    })
+  }
+
   render() {
     return (
       <section className="first-pet">
         <h4>Is this your first pet?</h4>
         <br />
         <form>
-          <input type="checkbox" />
+          <input 
+            type="checkbox" 
+            onChange={this.handleCheckbox}
+            checked={this.state.display}
+          />
         </form>
-        <h5></h5>
+        <h5>{ this.state.display ? "Yes" : "No" }</h5>
       </section>
     );
   }
