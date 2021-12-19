73291828822b4fdc2d21218166791bcbd2e70742
stephanie lucano <stephaniemarielucano@gmail.com>
2021-12-19 15:53:26 -0500

    Conditional rendering of text if checkbox is clicked

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./FirstPet.css";
+import { useState } from "react";
 
-class FirstPet extends React.Component {
-  render() {
-    return (
-      <section className="first-pet">
-        <h4>Is this your first pet?</h4>
-        <br />
-        <form>
-          <input type="checkbox" />
-        </form>
-        <h5></h5>
-      </section>
-    );
+const FirstPet = () => {
+  
+  const [isFirstPet, setIsFirstPet] = useState(false);
+
+  const handleClick = () => {
+    setIsFirstPet(!isFirstPet);
   }
+
+  return (
+    <section className="first-pet">
+      <h4>Is this your first pet?</h4>
+      <br />
+      <form>
+        <input type="checkbox" onClick={handleClick} />
+      </form>
+      <h5>{isFirstPet ? "Yes" : "No"}</h5>
+    </section>
+  );
 }
 
 export default FirstPet;
