08791d86cbdb6da78a393a38eb5ee0ac118cd142
stephanie lucano <stephaniemarielucano@gmail.com>
2021-12-19 16:46:59 -0500

    Add or remove types of animal from list

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,23 +1,51 @@
 import React from "react";
 import "./AnimalTypes.css";
+import { useState } from "react";
 
 const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
-class AnimalTypes extends React.Component {
-  render() {
-    return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol></ol>
-      </section>
-    );
+const AnimalTypes = () => {
+
+  const [ userInput, setUserInput ] = useState("");
+  const [ data, setData ] = useState(animalTypes)
+
+  const handleChange = (event) => {
+    setUserInput(event.target.value)
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setData([...data, userInput])
+    setUserInput("")
+  }
+
+  const handleRemove = (event) => {
+    setData(() => { 
+      return data.filter((type) => type !== event.target.value)
+    })
+  }
+
+  const handleMap = data.map((type) => {
+    return (
+      <li>
+        {type}
+        <button value={type} onClick={handleRemove}>-</button>
+      </li>
+    )
+  })
+
+  return (
+    <section className={"animal-types"}>
+      <h4>Animal Types</h4>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="type">
+          <input type="text" id="animal-type" value={userInput} onChange={handleChange}/>
+        </label>
+        <input type="submit" />
+      </form>
+      <ol>{handleMap}</ol>
+    </section>
+  );
 }
 
 export default AnimalTypes;
