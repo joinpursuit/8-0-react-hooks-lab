31ea04885ec71e6a11984290f3e8546966e8d9b5
Rose Durandisse <rosedurandisse@pursuit.org>
2021-12-19 13:06:00 -0500

    Updated Number of pets section:

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
+const NumberOfPets = () => {
+  const [count, setCount] = useState(0);
+
+  //if the count is 0, that state remain at 0, when the - button is clicked
+
+  const handleLessButton = () => {
+    if (count === 0) {
+      return;
+    } else {
+      setCount(count - 1)
+    }
   }
+  const handleMoreButton = () => {
+    setCount(count + 1)
+  }
+
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{count}</h5>
+      <div>
+        <button id="less" onClick={handleLessButton}>-</button>
+        <button id="more" onClick={handleMoreButton}>+</button>
+      </div>
+    </section>
+  );
 }
 
 export default NumberOfPets;
