87375f697ce8bc44cf5ed5d9a0d34a15f59c5937
stephanie lucano <stephaniemarielucano@gmail.com>
2021-12-19 16:13:12 -0500

    Poplate all the pets

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,37 @@
 import React from "react";
 import Pet from "./Pet";
 import "./PetList.css";
+import { useState, useEffect } from "react"
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://vet-api-8-1.herokuapp.com/api";
 
-class PetList extends React.Component {
-  render() {
+const PetList = () => {
+  
+  const [data, setData] = useState([])
+  
+  useEffect(() => {
+    fetch(`${BASE_URL}/pets`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+
+  const displayData = data.map((pet) => {
     return (
-      <section className="pet-list">
-        <h4>All Pets</h4>
-        <article></article>
-      </section>
-    );
-  }
+      <Pet 
+        key={pet.id} 
+        name={pet.name} 
+        kind={pet.kind} 
+        breed={pet.breed} 
+      />
+    )
+  })
+
+  return (
+    <section className="pet-list">
+      <h4>All Pets</h4>
+      <article>{displayData}</article>
+    </section>
+  );
 }
 
 export default PetList;
