3f7cd5f8869754c4c3edfa79ccd8eabc88b20401
stephanie lucano <stephaniemarielucano@gmail.com>
2021-12-19 15:47:11 -0500

    Added functionality to the minus and plus buttons

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,28 @@
 import React from "react";
 import "./NumberOfPets.css";
+import { useState } from "react"
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
+const NumberOfPets = () => {
+  const [numOfPets, setNumOfPets] = useState(0);
+  
+  const subtract = () => {
+    setNumOfPets(numOfPets - 1)
   }
+
+  const add = () => {
+    setNumOfPets(numOfPets + 1)
+  }
+  
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button id="less" onClick={subtract}>-</button>
+        <button id="more" onClick={add}>+</button>
+      </div>
+    </section>
+  );
 }
 
 export default NumberOfPets;
