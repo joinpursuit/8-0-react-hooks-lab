302fe1e73cc07ab31d4a48c50146e2b7bb3ef16f
Rose Durandisse <rosedurandisse@pursuit.org>
2021-12-19 15:37:17 -0500

    Included Fetch

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,37 @@
 import React from "react";
-import Pet from "./Pet";
+import { useState, useEffect } from "react";
+import Pet from "./Pet"
 import "./PetList.css";
 
 const BASE_URL = "http://localhost:5000/api";
 
-class PetList extends React.Component {
-  render() {
-    return (
-      <section className="pet-list">
-        <h4>All Pets</h4>
-        <article></article>
-      </section>
-    );
-  }
+const PetList = () => {
+
+  const [animal, setAnimal] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://vet-api-8-1.herokuapp.com/api/pets")
+      .then((response) => response.json())
+      .then((animals) => {
+        setAnimal(...animal, animals)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+
+  const eachAnimalDeets = animal.map((eachAnimal) => {
+    return <Pet name={eachAnimal.name} breed={eachAnimal.breed} kind={eachAnimal.kind} />
+  })
+
+
+  return (
+    <section className="pet-list">
+      <h4>All Pets</h4>
+      {eachAnimalDeets}
+    </section>
+  );
 }
+
 
 export default PetList;
