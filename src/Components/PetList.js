740eee3c03ba0d13240bf30d3dbc4bdf498b15f8
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-18 20:27:09 -0500

    PetList.js passes all tests using classes. No hooks

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -5,11 +5,43 @@ import "./PetList.css";
 const BASE_URL = "http://localhost:5000/api";
 
 class PetList extends React.Component {
+  constructor(){
+    super();
+
+    this.state={
+      animalData: [],
+    }
+  }
+
+  fetchAnimalData=()=>{
+    fetch("https://raw.githubusercontent.com/joinpursuit/resource-veterinarian-api/main/data/db.json")
+      .then(res=>res.json())
+      .then((data)=>{
+        this.setState({
+          animalData: data.pets,
+        })
+      })
+  }
+
+  componentDidMount=()=>{
+    this.fetchAnimalData();
+  }
+
   render() {
+    const petList = this.state.animalData.map((pet, index)=>{
+      return (
+        <div>
+          <h5>{pet.name}</h5>
+          <h5>{pet.breed}</h5>
+          <h5>{pet.kind}</h5>
+        </div>
+      )
+    })
+
     return (
       <section className="pet-list">
         <h4>All Pets</h4>
-        <article></article>
+        <article>{ petList }</article>
       </section>
     );
   }
