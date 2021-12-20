8c18e0a41e6c018188bbac51e2ac9d61919a426f
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-12-19 22:42:15 -0500

    updates api

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api/pets";
+const BASE_URL = "http://localhost:3000/api/pets";
 
 const PetList = () => {
   const [ petInfo, setPetInfo ] = useState([])
