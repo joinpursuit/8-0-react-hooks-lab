046e3c7fc3bbad3b4b27f44f38aed52d712cf296
Lakisha J <lakishajohnson@pursuit.org>
2021-12-20 14:06:06 -0500

    Number of Pets functional

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./NumberOfPets.css";
+import { useState } from "react"
 
-class NumberOfPets extends React.Component {
-  render() {
+const NumberOfPets = () => {
+  const [numOfPets, setNumOfPets] = useState(0)
+
+  const handleAddition = () => {
+    if(numOfPets >= 0) {
+      setNumOfPets(numOfPets + 1)
+    }
+  }
+
+  const handleDeduction = () => {
+    if(numOfPets > 0) {
+      setNumOfPets(numOfPets - 1)
+    }
+  }
+ 
     return (
       <section className="num-of-pets">
         <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
+        <h5>{numOfPets}</h5>
         <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
+          <button id="less" onClick={handleDeduction}>-</button>
+          <button id="more" onClick={handleAddition}>+</button>
         </div>
       </section>
     );
   }
-}
 
 export default NumberOfPets;
