7edc6aec2ef4b354254067beeed402857eae72ab
Xiaoming Yang <yangxiaoming295@gmail.com>
2021-12-20 14:06:48 -0500

    Modify Pet & PetList to function component

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
 const BASE_URL = "http://localhost:5000/api";
+const API = 'https://vet-api-8-1.herokuapp.com/api/pets';
 
-class PetList extends React.Component {
-  render() {
-    return (
-      <section className="pet-list">
-        <h4>All Pets</h4>
-        <article></article>
-      </section>
-    );
-  }
+function PetList() {
+  const [ petData, setPetData ] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => setPetData(data))
+  }, [])
+  return (
+    <section className="pet-list">
+      <h4>All Pets</h4>
+      <article>
+        {petData.map((pet) => <Pet name={pet.name} breed={pet.breed} kind={pet.kind} /> )}
+      </article>
+    </section>
+  );
 }
 
 export default PetList;
