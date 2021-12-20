c1497a7e57068f4a31484652a9fb34f8b6a85159
Lakisha J <lakishajohnson@pursuit.org>
2021-12-20 14:20:01 -0500

    New Client functional

--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -1,32 +1,43 @@
 import React from "react";
 import "./NewClient.css";
+import { useState } from "react"
 
-class NewClient extends React.Component {
-  render() {
+const NewClient = () => {
+  const [clientInfo, setClientInfo] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+  })
+
+  const handleUserInput = (event) => {
+    setClientInfo({ ...clientInfo, [event.target.id] : event.target.value})
+  }
+ 
     return (
       <section className="new-client">
         <h4>New Client Info</h4>
         <div>
           <form>
             <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" />
+            <input type="text" id="firstName" value={clientInfo.firstName} onChange={handleUserInput} />
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" />
+            <input type="text" id="lastName" value={clientInfo.lastName} onChange={handleUserInput} />
             <label htmlFor="phone">Telephone</label>
-            <input type="tel" id="phone" />
+            <input type="tel" id="phone" value={clientInfo.phone} onChange={handleUserInput} />
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" value={clientInfo.email} onChange={handleUserInput} />
           </form>
           <article>
-            <h5>firstName</h5>
-            <h5>lastName</h5>
-            <h5>phone</h5>
-            <h5>email</h5>
+            <h5>{clientInfo.firstName}</h5>
+            <h5>{clientInfo.lastName}</h5>
+            <h5>{clientInfo.phone}</h5>
+            <h5>{clientInfo.email}</h5>
           </article>
         </div>
       </section>
     );
   }
-}
+
 
 export default NewClient;
