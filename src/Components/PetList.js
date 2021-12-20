548aa8a5077fb127511a6478a600bf3dac66df89
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-12-19 22:37:10 -0500

    changes to functional component & fetch api

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://localhost:5000/api/pets";
 
-class PetList extends React.Component {
-  render() {
+const PetList = () => {
+  const [ petInfo, setPetInfo ] = useState([])
+  
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then((response) => response.json())
+    .then((data) => setPetInfo(data))
+    .catch((error) => { 
+      throw error })
+    },[])
+
+  let Pets = petInfo.map((pet) => <Pet name={pet.name} breed={pet.breed} kind={pet.kind} />)
+
     return (
       <section className="pet-list">
         <h4>All Pets</h4>
-        <article></article>
+        <article>{Pets}</article>
       </section>
     );
   }
-}
+
 
 export default PetList;
