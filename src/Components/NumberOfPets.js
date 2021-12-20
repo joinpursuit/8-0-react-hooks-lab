2d830071ea19149dff6c96fb146ccc3df05ea6c6
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-12-19 19:36:06 -0500

    makes add and remove method

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
+const NumberOfPets = () => {
+  let [numOfPets, setNumOfPets] = useState(0);
+
+  const handleAddPet = () => { 
+    setNumOfPets(++numOfPets) 
   }
+
+  const handleMinusPet = () => {
+    if(numOfPets > 0) {
+      numOfPets = --numOfPets;
+    } 
+    setNumOfPets(numOfPets)
+  }
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button id="less" onClick={handleMinusPet}>-</button>
+        <button id="more" onClick={handleAddPet}>+</button>
+      </div>
+    </section>
+  );
 }
 
 export default NumberOfPets;
