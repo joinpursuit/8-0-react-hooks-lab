f0c23eb3e8203343a2fbd757965021b514b31923
Xiaoming Yang <yangxiaoming295@gmail.com>
2021-12-19 22:31:37 -0500

    Modify NavBar, Footer & FirstPet to functional component

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
-    return (
+
+function FirstPet() {
+  const [ firstPet, setFirstPet ] = useState(false);
+
+  const handleCheckBox = () => { setFirstPet(!firstPet) }
+  return (
       <section className="first-pet">
         <h4>Is this your first pet?</h4>
         <br />
         <form>
-          <input type="checkbox" />
+          <input type="checkbox" onChange={handleCheckBox} />
         </form>
-        <h5></h5>
+        <h5>{firstPet ? 'Yes' : 'No'}</h5>
       </section>
-    );
-  }
+  );
 }
 
 export default FirstPet;
