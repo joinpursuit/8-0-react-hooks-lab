23556d31d938899b1612ea05f93c6610879011be
Xiaoming Yang <yangxiaoming295@gmail.com>
2021-12-19 22:59:33 -0500

    Modify NumberOfPets to function component

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
+function NumberOfPets() {
+  let [ numberOfPets, setNumberOfPets ] = useState(0);
+
+  const handleMinusPet = () => {
+    numberOfPets = (numberOfPets > 0 ? --numberOfPets : 0);
+    setNumberOfPets(numberOfPets);
   }
+
+  const handleAddPet = () => { setNumberOfPets(++numberOfPets) }
+
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numberOfPets}</h5>
+      <div>
+        <button id="less" onClick={handleMinusPet}>-</button>
+        <button id="more" onClick={handleAddPet}>+</button>
+      </div>
+    </section>
+  );
 }
 
 export default NumberOfPets;
