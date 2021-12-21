c01cc0bc4c60ab434462499228b74276915bd935
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-20 23:50:21 -0500

    AnimalTypes.js half-converted into hooks. Logic does not work to pass the tests. For both classes and hooks. TO DO: fix AnimalTypes.js

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,59 +1,46 @@
 import React from "react";
 import "./AnimalTypes.css";
+import { useState } from 'react';
 
 const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
-class AnimalTypes extends React.Component {
-  constructor(){
-    super();
+const AnimalTypes=()=>{
+  const [ newAnimalArr, setNewAnimalArr ] = useState([]);
+  const [ userInput, setUserInput ] = useState('')
 
-    this.state = {
-      newAnimalArr: [],
-      userInput: '',
-    }
-  }
-
-  handleInput=(event)=>{
+  const handleInput=(event)=>{
     event.preventDefault();
-    this.setState({
-      userInput: event.target.value,
-    })
+    setUserInput(event.target.value) 
   }
 
-  // addToList=(event)=>{
-  //   this.setState({
-  //     animalArr: this.state.animalArr + event.target.value
-  //   })
+  // const addToList=(event)=>{
+  //   setNewAnimalArr(animalArr + event.target.value)
   // }
 
-  removeButton=(event)=>{
-    this.setState({
-      animalArr: event.target.value,
-    })
+  const removeButton=(event)=>{
+    setNewAnimalArr(event.target.value)
   }
 
-  render() {
-    const animalList = animalTypes.map((animal, index)=>{
-      return (
-        <div key={index}>
-          <li>{animal}<button onClick={this.removeButton}>-</button></li>
-        </div>
-      )
-    })
-
+  const animalList = animalTypes.map((animal, index)=>{
     return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form onClick={this.handleInput}>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol>{animalList}</ol>
-      </section>
-    );
-  }
+      <div key={index}>
+        <li>{animal}<button onClick={(e)=>removeButton(e)}>-</button></li>
+      </div>
+    )
+  })
+
+  return (
+    <section className={"animal-types"}>
+      <h4>Animal Types</h4>
+      <form onClick={(e)=>handleInput(e)}>
+        <label htmlFor="type">
+          <input type="text" id="animal-type" />
+        </label>
+        <input type="submit" />
+      </form>
+      <ol>{animalList}</ol>
+    </section>
+  );
 }
 
 export default AnimalTypes;
