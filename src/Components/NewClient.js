2a4dedb5e071ab7163e67d532595ec1ec7eb6254
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-12-21 17:48:44 -0500

    made necessary changes

--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -1,32 +1,68 @@
-import React from "react";
+import { useState } from "react";
 import "./NewClient.css";
 
-class NewClient extends React.Component {
-  render() {
-    return (
-      <section className="new-client">
-        <h4>New Client Info</h4>
-        <div>
-          <form>
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" />
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" />
-            <label htmlFor="phone">Telephone</label>
-            <input type="tel" id="phone" />
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
-          </form>
-          <article>
-            <h5>firstName</h5>
-            <h5>lastName</h5>
-            <h5>phone</h5>
-            <h5>email</h5>
-          </article>
-        </div>
-      </section>
-    );
-  }
-}
+const NewClient = () => {
+  const [state, setState] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    phoneNbr: "",
+  });
+
+  const handleChange = (e) => {
+    setState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const { fName, lName, phoneNbr, email } = state;
+  return (
+    <section className="new-client">
+      <h4>New Client Info</h4>
+      <div>
+        <form>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            id="firstName"
+            name="fName"
+            value={fName}
+            onChange={handleChange}
+          />
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lName"
+            value={lName}
+            onChange={handleChange}
+          />
+          <label htmlFor="phone">Telephone</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phoneNbr"
+            value={phoneNbr}
+            onChange={handleChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </form>
+        <article>
+          <h5>{fName}</h5>
+          <h5>{lName}</h5>
+          <h5>{phoneNbr}</h5>
+          <h5>{email}</h5>
+        </article>
+      </div>
+    </section>
+  );
+};
 
 export default NewClient;
