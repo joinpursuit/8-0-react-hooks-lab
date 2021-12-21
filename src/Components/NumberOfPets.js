6a1a351bf3cfd23b06d333dd106466519a18f0c7
Charlie Moran <Carlosamoran18@gmail.com>
2021-12-21 11:18:43 -0500

    remove add pet done

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
+const NumberOfPets = () => {
+  let [numPets, setNumPets] = useState(0);
+  const petAdd = () => {setNumPets(++numPets)}
+  const removePet = () => {
+    numPets = (numPets > 0 ? --numPets : 0);
+    setNumPets(numPets);
+  } 
     return (
       <section className="num-of-pets">
         <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
+        <h5>{numPets}</h5>
         <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
+          <button id="less" onClick={removePet}>-</button>
+          <button id="more" onClick={petAdd}>+</button>
         </div>
       </section>
     );
-  }
 }
 
 export default NumberOfPets;
