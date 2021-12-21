ecf900a9d57fe57d929f3b6934501e73d4208bfb
Charlie Moran <Carlosamoran18@gmail.com>
2021-12-21 11:34:40 -0500

    pet list done

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://vet-api-8-1.herokuapp.com/api/pets";
 
-class PetList extends React.Component {
-  render() {
+const PetList = () => {
+  const [petData, setPetData] = useState([])
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then((res) => res.json())
+    .then((data) => setPetData(data))
+    .catch((err) => {throw err})
+  }, [])
     return (
       <section className="pet-list">
         <h4>All Pets</h4>
-        <article></article>
+        <article>
+          {petData.map((pet) => <Pet name={pet.name} breed={pet.breed} kind={pet.kind} /> )}
+        </article>
       </section>
     );
-  }
 }
 
 export default PetList;
