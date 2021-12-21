f16ffb39d0c22ce10f7171fa6faf64c9d6400467
rae charles <r_charles87@yahoo.com>
2021-12-21 11:10:58 -0500

    finshed number of pets

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,34 @@
-import React from "react";
+import {React, useState} from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
+function NumberOfPets () {
+
+  const [count, setcount] = useState(0)
+
+  const handleIncrement=()=>{
+    setcount(count + 1)
+  }
+
+  const handleDecrement=()=>{
+    setcount(count - 1)
+  }
+
+
+  // const handleCountChange=()=>{
+
+  // }
+
     return (
       <section className="num-of-pets">
         <h4>Number of Pets</h4>
         <h5>numOfPets</h5>
+        <h5>{count}</h5>
         <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
+          <button id="less" onClick={handleDecrement}>-</button>
+          <button id="more" onClick={handleIncrement}>+</button>
         </div>
       </section>
     );
-  }
 }
 
 export default NumberOfPets;
