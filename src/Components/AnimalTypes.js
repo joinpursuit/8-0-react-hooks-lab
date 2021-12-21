63af6c5d4fdf7893ba71662499285760c6b592bd
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-12-20 22:59:08 -0500

    worked with sal.

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -2,34 +2,46 @@ import React, {useState} from "react";
 import "./AnimalTypes.css";
 
 
-const AnimalTypes = () => {
-  
-  const [handleForm, setHandleForm ] = useState(
-    { 
+      const AnimalTypes = () => {
+
+      const [handleForm, setHandleForm ] = useState(
+      { 
       animalTypes:["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"],
       userInput: ""
-    })  ;
+      })  ;//making a list for each type of animal
 
-  let results = "";
-    //making a list for each type of animal
+      const handleInput = (event) => {
+
+      setHandleForm({...handleForm, [event.target.name]: event.target.value.trim().toLowerCase()})
+      //event.target.name is the name of the state
+      }
   
-  const handleInput = (event) => {
-  setHandleForm({...handleForm, [event.target.name]: event.target.value.trim().toLowerCase()})
-  }
-  
-  const afterSubmit = (event) => {
+    const afterSubmit = (event) => {
     event.preventDefault(); //prevent refresh
     //checking to see if the animal type = userinput
     //if false, input isnt added to array
     //if true, input is added to the array, if
-    !handleForm.animalTypes.some((type) => type === handleForm.userInput && handleForm.animalTypes.push(handleForm.userInput))
-    
+    !handleForm.animalTypes.some((type) => type === handleForm.userInput) && handleForm.animalTypes.push(handleForm.userInput)
+    //adding animal type. if it exists, animal isn't added [no duplicates]
+
     setHandleForm({...handleForm, animalTypes: handleForm.animalTypes}) //new user input becomes a part of animal types
 
-    results = animalTypes.map((type, i) => <li onClick={handleDelete} key={i}>{type}</li>)
+    event.target.reset(); //reset forms
+    }
 
-    event.target.reset();
+  const handleDelete = (event) => {
+  const deletedType = event.target.parentNode.innerText.split('\n')[1] //[delete], [animaltype] --> txt that gets deleted
+  setHandleForm({...handleForm, animalTypes: handleForm.animalTypes.filter((type)=> type !== deletedType)})
+  //looking to see if deletedType exists within the elements of the array
+  //if it exists, filter it out.
+  //return the array without the animal type
+  //updates the state: animalTypes
+  console.log(handleForm.animalTypes.filter((type)=> type !== deletedType))
   }
+  
+  const results = handleForm.animalTypes.map((type, i) => <li key={i}><button onClick={handleDelete}>delete</button>{type}</li>)
+  //<li> is a parent of button
+  //console.dir
 
 
   return (
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
 const BASE_URL = "https://vet-api-8-1.herokuapp.com";
 
 const PetList = () => {
+  
+  const [allPets, setAllPets] = useState([]);
+  
+  useEffect(() => {
+  fetch(BASE_URL + '/api/pets')
+  .then(response => response.json())
+  .then((pets) => setAllPets(pets))
+  //fetching info from api then taking an array of obj in order to set the states
+    
+  }, []) //empty brackets prevent useEffect from constantly calling itself, works similar to componentDidMount().
+
+  const results = allPets.map(({name, breed, kind, id}) => <Pet key={id} name={name} breed={breed} kind={kind} />)
+  //destructuring
+  //passing pet as a prop, setting key then using info from pet.js
 
     return (
       <section className="pet-list">
         <h4>All Pets</h4>
-        <article></article>
+        <article>{results}</article>
       </section>
     );
-  
 }
 
 export default PetList;
