eff1140d5a6200967f7e334ced3d9e4fdfc8b3f7
rae charles <r_charles87@yahoo.com>
2021-12-21 10:59:52 -0500

    finshed navbar, firstpet and footer

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,27 @@
-import React from "react";
+import { React, useState } from "react";
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
+ function FirstPet () {
+   const [firstPet, setFirstPet] = useState(false)
+
+   const handleCheckbox=()=>{
+    setFirstPet(!firstPet)
+   }
+
     return (
       <section className="first-pet">
         <h4>Is this your first pet?</h4>
         <br />
-        <form>
-          <input type="checkbox" />
+        <form onChange={handleCheckbox}>
+          <input 
+          type="checkbox" 
+          value={firstPet}
+          checked={firstPet}
+          />
         </form>
-        <h5></h5>
+        <h5>{firstPet? "Yes": "No"}</h5>
       </section>
     );
-  }
 }
 
 export default FirstPet;
