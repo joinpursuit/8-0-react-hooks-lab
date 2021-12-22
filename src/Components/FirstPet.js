86da5071b741365765c5975aee369c3452fa6d35
Sara D'Arce <saradarce@gmail.com>
2021-12-21 22:55:08 -0500

    had lots of help will study

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./FirstPet.css";
+import { useState } from "react";
 
-class FirstPet extends React.Component {
-  render() {
-    return (
-      <section className="first-pet">
-        <h4>Is this your first pet?</h4>
-        <br />
-        <form>
-          <input type="checkbox" />
-        </form>
-        <h5></h5>
-      </section>
-    );
-  }
-}
+const FirstPet = () => {
+  const [isFirstPet, setIsFirstPet] = useState(false);
+
+  const handleChange = () => {
+    setIsFirstPet(!isFirstPet);
+  };
+
+  return (
+    <section className="first-pet">
+      <h4>Is this your first pet?</h4>
+      <br />
+      <form>
+        <input type="checkbox" onChange={handleChange} />
+      </form>
+      <h5>{isFirstPet ? <p>Yes</p> : <p>No</p>}</h5>
+    </section>
+  );
+};
 
 export default FirstPet;
--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,33 @@
 import React from "react";
 import "./NumberOfPets.css";
+import { useState } from "react";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
-  }
-}
+const NumberOfPets = () => {
+  const [numOfPets, setNumOfPets] = useState(0);
+
+  const addPet = () => {
+    setNumOfPets(numOfPets + 1);
+  };
+
+  const subtractPet = () => {
+    if (numOfPets < 1) return;
+    setNumOfPets(numOfPets - 1);
+  };
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button id="less" onClick={subtractPet}>
+          -
+        </button>
+        <button id="more" onClick={addPet}>
+          +
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default NumberOfPets;
