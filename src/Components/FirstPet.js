bb3f48ebf92dcdeead0bf6ced12b818d38b2f812
Deborah Campos <631care@gmail.com>
2021-12-23 11:43:17 -0500

    initial conversions

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,38 @@
 import React from "react";
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
-    return (
+const FirstPet=()=>{
+  const[firstPet, setPet] = useState(false)
+  
+  handleCheckBox=()=>{
+
+  }
+
+  return (
       <section className="first-pet">
-        <h4>Is this your first pet?</h4>
-        <br />
-        <form>
-          <input type="checkbox" />
-        </form>
-        <h5></h5>
+          <h4>Is this your first pet?</h4>
+          <br />
+          <form>
+            <input type="checkbox" onChange={handleCheckBox}/>
+          </form>
+          <h5></h5>
       </section>
     );
-  }
 }
+
+// class FirstPet extends React.Component {
+//   render() {
+//     return (
+//       <section className="first-pet">
+//         <h4>Is this your first pet?</h4>
+//         <br />
+//         <form>
+//           <input type="checkbox" />
+//         </form>
+//         <h5></h5>
+//       </section>
+//     );
+//   }
+// }
 
 export default FirstPet;
