8c6b7dfa057fd65657225687648986e267eff1ea
Deborah Campos <631care@gmail.com>
2022-01-03 13:34:40 -0500

    petList test not passing despite rendering correctly

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,42 @@
 import React from "react";
 import Pet from "./Pet";
 import "./PetList.css";
+import { useEffect, useState } from "react";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://vet-api-8-1.herokuapp.com/api/pets";
 
-class PetList extends React.Component {
-  render() {
-    return (
+const PetList=()=>{
+  const[petList, setpetList]= useState([])
+
+    useEffect(()=>{
+      handleFetch();
+    },[]);
+    const handleFetch=()=>{
+      fetch(BASE_URL)
+      .then((res)=> res.json())
+      .then(data =>{
+        setpetList(data)
+      })
+    }
+    let pets = petList.map((pet)=>{
+      return (
+        <div>
+          <Pet
+            key={pet.id}
+            name={pet.name}
+            breed={pet.breed}
+            kind={pet.kind}
+          />
+        </div>
+      )
+    })
+      return (
       <section className="pet-list">
         <h4>All Pets</h4>
-        <article></article>
+          {pets}
       </section>
     );
-  }
 }
+
 
 export default PetList;
