2a3f507519ed88b2d582f9d5a3f2e03d69b44910
Spencer Simon <spencersimon@pursuit.org>
2022-06-28 17:51:26 -0400

    React Hooks Lab

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,23 +1,54 @@
-import React from "react";
+import { useState } from "react";
 import "./AnimalTypes.css";
 
 const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
-class AnimalTypes extends React.Component {
-  render() {
+const AnimalTypes = () => {
+  const [input, setInput] = useState("");
+  const [allInputs, setAllInputs] = useState(animalTypes);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!allInputs.includes(input)) {
+      setAllInputs([...allInputs, input]);
+    }
+  };
+
+  const handleDelete = (event) => {
+    setAllInputs(() => {
+      return allInputs.filter((input) => input !== event.target.id);
+    });
+  };
+
+  const listOfAnimals = allInputs.map((animal) => {
     return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol></ol>
-      </section>
+      <li>
+        {animal}
+        <button onClick={handleDelete} id={animal}>
+          {" "}
+          Remove
+        </button>
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <section className={"animal-types"}>
+      <h4>Animal Types</h4>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="type">
+          <input
+            type="text"
+            id="animal-type"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <ol> {listOfAnimals} </ol>
+    </section>
+  );
+};
 
 export default AnimalTypes;
