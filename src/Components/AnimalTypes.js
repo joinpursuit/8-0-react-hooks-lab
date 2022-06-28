8dd67793eaede10da4ed6d03d0346b28b6a98232
arupay <arupay11374@gmail.com>
2022-06-28 19:22:27 -0400

    completed animal types , no duplicates, remove button included

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -4,18 +4,44 @@ import "./AnimalTypes.css";
 const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
 const AnimalTypes = () => {
-  const [animal, setAnimal] = useState([]);
+  const [animals, setAnimals] = useState([...animalTypes]);
+  const [animal, setAnimal] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!animals.includes(animal)) setAnimals((prev) => [...prev, animal]);
+  };
 
   return (
     <section className={"animal-types"}>
       <h4>Animal Types</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="type">
-          <input type="text" id="animal-type" />
+          <input
+            type="text"
+            id="animal-type"
+            value={animal}
+            onChange={({ target }) => setAnimal(target.value.toLowerCase())}
+          />
         </label>
         <input type="submit" />
       </form>
-      <ol></ol>
+      <ol>
+        {animals.map((animal, index) => {
+          return (
+            <li key={index}>
+              {animal}
+              <button
+                onClick={() =>
+                  setAnimals((prev) => prev.filter((e) => e !== animal))
+                }
+              >
+                Remove
+              </button>
+            </li>
+          );
+        })}
+      </ol>
     </section>
   );
 };
