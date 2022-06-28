58e569761456084a4a482a34ed43f605fcaf86f8
JuHaoChen1997 <juhaochen97@gmail.com>
2022-06-28 12:47:05 -0400

    finish should allow a new client to type information

--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -2,25 +2,46 @@ import { useState } from "react";
 import "./NewClient.css";
 
 function NewClient() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <section className="new-client">
       <h4>New Client Info</h4>
       <div>
         <form>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" id="firstName" />
+          <input
+            type="text"
+            id="firstName"
+            onChange={(event) => setFirstName(event.target.value)}
+          />
           <label htmlFor="lastName">Last Name</label>
-          <input type="text" id="lastName" />
+          <input
+            type="text"
+            id="lastName"
+            onChange={(event) => setLastName(event.target.value)}
+          />
           <label htmlFor="phone">Telephone</label>
-          <input type="tel" id="phone" />
+          <input
+            type="tel"
+            id="phone"
+            onChange={(event) => setPhone(event.target.value)}
+          />
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            id="email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </form>
         <article>
-          <h5>firstName</h5>
-          <h5>lastName</h5>
-          <h5>phone</h5>
-          <h5>email</h5>
+          <h5>{firstName}</h5>
+          <h5>{lastName}</h5>
+          <h5>{phone}</h5>
+          <h5>{email}</h5>
         </article>
       </div>
     </section>
