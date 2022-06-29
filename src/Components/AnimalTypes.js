d0b15c6bcb7efa79d71c163e19313b492e3032e5
T.K <nique@Niques-MacBook-Air.local>
2022-06-29 16:36:43 -0400

    finishing lab

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,19 +1,56 @@
 import React from "react";
+import { useState } from "react";
 import "./AnimalTypes.css";
 
 const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
 export default function AnimalTypes() {
+  const [input, setInput] = useState("");
+  const [animalList, setAnimalList] = useState(animalTypes);
+
+  const handleChangeInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!animalList.includes(input)) {
+      setAnimalList([...animalList, input])
+    }
+  };
+
+  const handleDelete = (e) => {
+    setAnimalList(() => {
+      return animalList.filter((input) => input !== e.target.id);
+    });
+  };
+
+  const animalMap = animalList.map((animal) => {
+      return (
+        <li>
+          {animal}
+          <button onClick={handleDelete} id={animal}>
+            Delete
+          </button>
+        </li>
+      );
+  });
+
   return (
     <section className={"animal-types"}>
       <h4>Animal Types</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="type">
-          <input type="text" id="animal-type" />
+          <input
+            type="text"
+            id="animal-type"
+            onChange={handleChangeInput}
+            value={input}
+          />
         </label>
         <input type="submit" />
       </form>
-      <ol></ol>
+      <ol>{animalMap}</ol>
     </section>
   );
 }
