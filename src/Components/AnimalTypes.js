eda32478a97a2fadbf2163c147656e587f7db3f8
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-06-28 23:39:21 -0400

    completed lab

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,23 +1,71 @@
-import React from "react";
+import { useState } from "react";
 import "./AnimalTypes.css";
 
-const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
+const AnimalTypes = () => {
+  const [animalType, setAnimalType] = useState("");
 
-class AnimalTypes extends React.Component {
-  render() {
-    return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol></ol>
-      </section>
-    );
-  }
-}
+  const [animalTypes, setAnimalTypes] = useState([
+    "dog",
+    "cat",
+    "ferret",
+    "bird",
+    "fish",
+    "snake",
+    "lizard",
+  ]);
+
+  const renderAnimalTypes = () => {
+    let keyNum = 0;
+
+    return animalTypes.map((animal) => {
+      return (
+        <li key={keyNum++}>
+          {animal}
+          <button
+            onClick={() => {
+              deleteFromAnimalTypes(animal);
+            }}
+          >
+            -
+          </button>
+        </li>
+      );
+    });
+  };
+
+  const addToAnimalTypes = (e) => {
+    e.preventDefault();
+
+    if (animalType === "" || animalTypes.includes(animalType)) {
+      setAnimalType("");
+      return;
+    } else {
+      setAnimalTypes([...animalTypes, animalType]);
+      setAnimalType("");
+    }
+  };
+
+  const deleteFromAnimalTypes = (animal) => {
+    setAnimalTypes(animalTypes.filter((type) => type !== animal));
+  };
+
+  return (
+    <section className={"animal-types"}>
+      <h4>Animal Types</h4>
+      <form onSubmit={(e) => addToAnimalTypes(e)}>
+        <label htmlFor="type">
+          <input
+            type="text"
+            id="animal-type"
+            value={animalType}
+            onChange={(e) => setAnimalType(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <ol>{renderAnimalTypes()}</ol>
+    </section>
+  );
+};
 
 export default AnimalTypes;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,49 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = `http://serene-tundra-77911.herokuapp.com/api/pets`;
 
-class PetList extends React.Component {
-  render() {
-    return (
-      <section className="pet-list">
-        <h4>All Pets</h4>
-        <article></article>
-      </section>
-    );
-  }
-}
+const PetList = () => {
+  const [pets, setPets] = useState([]);
+  const [error, setError] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(BASE_URL)
+      .then((res) => {
+        console.log(res.data);
+        setPets([...res.data]);
+        renderPets(pets);
+      })
+      .catch((err) => {
+        errors(err);
+      });
+  }, []); // eslint-disable-line
+
+  const renderPets = (pets) => {
+    const petList = pets.map((pet) => {
+      return (
+        <Pet key={pet.id} name={pet.name} breed={pet.breed} kind={pet.kind} />
+      );
+    });
+    return petList;
+  };
+
+  const errors = (err) => {
+    setError(err);
+  };
+
+  return (
+    <section className="pet-list">
+      <h4>All Pets</h4>
+      <article>
+        {error && <p>{error}</p>}
+        {renderPets(pets)}
+      </article>
+    </section>
+  );
+};
 
 export default PetList;
