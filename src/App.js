7ec8839b1f56e2a8b773e8a312f59af6f713045f
ziqian pan <ziqianpan1998@gmail.com>
2022-06-28 20:00:27 -0400

    3/7 - 4 fails

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AnimalTypes from "./Components/AnimalTypes";
 import FirstPet from "./Components/FirstPet";
 import Footer from "./Components/Footer";
@@ -6,8 +6,24 @@ import NavBar from "./Components/NavBar";
 import NewClient from "./Components/NewClient";
 import NumberOfPets from "./Components/NumberOfPets";
 import PetList from "./Components/PetList";
+const BASE_URL = "https://vet-lab-8-4.herokuapp.com/api";
 
 const App = () => {
+  const [allPets, setAllPets] = useState([]);
+
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/pets`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllPets(data);
+       
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <NavBar />
@@ -16,7 +32,7 @@ const App = () => {
         <NumberOfPets />
         <NewClient />
         <AnimalTypes />
-        <PetList />
+        <PetList allPets={allPets} />
       </main>
       <Footer />
     </>
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api";
+
+
+export default function PetList({allPets}) {
+
+  return (
+    <section className="pet-list">
+      {/* { console.log(allPets)} */}
+      <h4>All Pets</h4>
+
+      <article>
+        {" "}
+        {allPets.map((pet) => {
+          return <Pet name={pet.name} kind={pet.kind} breed={pet.breed} key={pet.id}/>;
+        })}
+      </article>
+    </section>
+  );
+}
 
 // class PetList extends React.Component {
 //   render() {
@@ -14,14 +31,3 @@ const BASE_URL = "http://localhost:5000/api";
 //     );
 //   }
 // }
-
-export default function PetList() {
-  return (
-    <section className="pet-list">
-             <h4>All Pets</h4>
-             {/* render pet.js */}
-            <article></article>
-    </section>
-  )
-}
-
