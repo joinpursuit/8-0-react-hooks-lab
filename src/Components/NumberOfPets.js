de50a65e2d9ac31509f73730576424523f661a5a
T.K <nique@Niques-MacBook-Air.local>
2022-06-29 13:08:02 -0400

    fixing numberofpets.js

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,28 +1,33 @@
 import React from "react";
 import "./NumberOfPets.css";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function NumberOfPets() {
+  let [num, setNum] = useState(0);
 
-  // let [num, setNum] = useState(0);
+  let handleClickAdd = (e) => {
+    setNum((num += 1));
+  };
 
-  let handleClick = (e) => {
-    
-  }
-
+  let handleClickSubtract = (e) => {
+    setNum((num -= 1 && num > 0));
+  };
 
   return (
     <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less" onClick={ handleClick }>-</button>
-          <button id="more" onClick={ handleClick }>+</button>
-        </div>
-      </section>
-  )
+      <h4>Number of Pets</h4>
+      <h5>{num}</h5>
+      <div>
+        <button id="less" onClick={handleClickSubtract}>
+          -
+        </button>
+        <button id="more" onClick={handleClickAdd}>
+          +
+        </button>
+      </div>
+    </section>
+  );
 }
-
 
 // class NumberOfPets extends React.Component {
 //   render() {
