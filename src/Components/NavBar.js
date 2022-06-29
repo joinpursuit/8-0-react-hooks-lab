d4b6c8e88f88353c7c22cd7f71d79898a399be1c
Khaleed Uddin <tashrif@Khaleeds-MacBook-Air-2.local>
2022-06-28 20:52:58 -0400

    ...

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./NavBar.css";
 
-export class NavBar extends React.Component {
-  render() {
+ const NavBar = () => {
+  
     return (
       <nav>
         <h1>Northwest Animal Hospital</h1>
         <h2>New Client Sign-up</h2>
       </nav>
     );
-  }
 }
 
 export default NavBar;
--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
+const NumberOfPets = () => {
+const [numPets, setNumPets] = useState(0)
+
+
+const handleMinus = () => {
+  setNumPets(numPets - 1)
+
+  if(numPets <= 0){
+    setNumPets(0)
+  }
+}
+
+const handlePlus = () => {
+setNumPets(numPets + 1)
+}
+
+
     return (
       <section className="num-of-pets">
         <h4>Number of Pets</h4>
         <h5>numOfPets</h5>
         <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
+          <button onClick={handleMinus} id="less">-</button>
+          <p>{numPets}</p>
+          <button onClick={handlePlus} id="more">+</button>
         </div>
       </section>
     );
   }
-}
+
 
 export default NumberOfPets;
