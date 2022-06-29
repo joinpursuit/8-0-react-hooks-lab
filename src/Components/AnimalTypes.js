6a60ed3f4f99a46b9acb60ecaa679886ba61f5c5
Christine Taylor <ctay47@icloud.com>
2022-06-29 19:00:22 -0400

    completed hooks lab

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,23 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import "./AnimalTypes.css";
 
-const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
+const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"]
 
-class AnimalTypes extends React.Component {
-  render() {
-    return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol></ol>
-      </section>
-    );
+const AnimalTypes = () => {
+
+  const [animals, setAnimals] = useState({
+    userInput: '',
+    animalTypes,
+  })
+
+  const handleChange = (event) => {
+    const { value } = event.target
+    setAnimals({...animals, userInput: value})
   }
+
+  const handleRemove = (event) => {
+    const {animalTypes: types} = animals
+    const { id } = event.target
+    setAnimals({...animals, animalTypes: types.filter(animal => animal !== types[id])})
+  }
+
+  const handleAdd = (event) => {
+    event.preventDefault()
+    const {animalTypes: types, userInput} = animals
+    setAnimals({...animals, animalTypes: [...types, userInput]})
+  }
+
+  return (
+    <section className={"animal-types"}>
+      <h4>Animal Types</h4>
+      <form onSubmit={handleAdd}>
+        <label htmlFor="type">
+          <input onChange={handleChange} type="text" id="animal-type" />
+        </label>
+        <input type="submit" />
+      </form>
+      <ol>{animals.animalTypes.map((animal, index) =>  <li key={index}>{animal}<button id={index} onClick={handleRemove}></button></li>)}</ol>
+    </section>
+  );
 }
 
 export default AnimalTypes;
+
+
+
+
+
+
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,11 @@
-import React from "react";
-export default class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <h4>Footer</h4>
-      </footer>
-    );
-  }
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <h4>Footer</h4>
+    </footer>
+  );
 }
+
+export default Footer
