a776ee5841efd971aff8d04da9311c0e14ad8851
jede <jedebrahma@pursuit.org>
2022-06-28 21:06:00 -0400

    hooks-zane

--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewClient.css";
 
-class NewClient extends React.Component {
-  render() {
-    return (
-      <section className="new-client">
-        <h4>New Client Info</h4>
-        <div>
-          <form>
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" />
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" />
-            <label htmlFor="phone">Telephone</label>
-            <input type="tel" id="phone" />
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
-          </form>
-          <article>
-            <h5>firstName</h5>
-            <h5>lastName</h5>
-            <h5>phone</h5>
-            <h5>email</h5>
-          </article>
-        </div>
-      </section>
-    );
-  }
-}
+const NewClient = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <section className="new-client">
+      <h4>New Client Info</h4>
+      <div>
+        <form>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            type="text"
+            id="firstName"
+          />
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            type="text"
+            id="lastName"
+          />
+          <label htmlFor="phone">Telephone</label>
+          <input
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            type="tel"
+            id="phone"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id="email"
+          />
+        </form>
+        <article>
+          <h5>{firstName}</h5>
+          <h5>{lastName}</h5>
+          <h5>{phone}</h5>
+          <h5>{email}</h5>
+        </article>
+      </div>
+    </section>
+  );
+};
 
 export default NewClient;
--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
+const NumberOfPets = () => {
+  const [numberOfPets, setNumberOfPets] = useState(0);
+  const handleLess =() => {
+    setNumberOfPets(numberOfPets-1)
+  };
+
+  const handleMore =() => {
+    setNumberOfPets(numberOfPets+1)
+  };
     return (
       <section className="num-of-pets">
         <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
+        <h5>{numberOfPets}</h5>
         <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
+          <button onClick = {handleLess} id="less">-</button>
+          <button onClick = {handleMore} id="more">+</button>
         </div>
       </section>
     );
   }
-}
 
 export default NumberOfPets;
