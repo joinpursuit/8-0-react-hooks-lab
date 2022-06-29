553835337433f9c5f3354c80f37e18a9842af566
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-06-29 11:06:40 -0400

    finished

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
 import "./AnimalTypes.css";
 
-const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
+let animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
 const AnimalTypes = () =>{
+  const [animalTypesArr, setAnimalTypeArr] = useState(animalTypes)
+  const [input, setInput] = useState("");
   
+
+  const handleDelete = (e) => {
+    const newList = animalTypesArr.filter((animal) => 
+      animal !== e.target.parentNode.textContent.split("")[0]
+    )
+    setAnimalTypeArr(newList)
+  }
+
+  const handleChange = (e) => {
+    const {value} = e.target
+    setInput(value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setAnimalTypeArr(oldArr => [...oldArr, input])
+  }
+
+
     return (
       <section className={"animal-types"}>
         <h4>Animal Types</h4>
-        <form>
+        <ol>
+          {animalTypesArr.map((animalType) => {
+            return (
+              <>
+              <li>{animalType} <button onClick={handleDelete}>Delete</button>
+              </li>
+              </>
+            )
+          })}
+        </ol>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="type">
-            <input type="text" id="animal-type" />
+            <input onChange={handleChange} type="text" id="animal-type" />
           </label>
           <input type="submit" />
         </form>
