e4110f4ac45c64b7992a3a7504fd308d32894d8a
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-06-28 20:16:09 -0400

    3 components done

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,24 @@
 import React from "react";
+import {useState} from "react";
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
+const FirstPet = () => {
+  const [checked, setChecked] = useState(false);
+  
+  const handlePet = () =>{
+    checked ? setChecked(false) : setChecked(true)
+  }
+  
     return (
       <section className="first-pet">
         <h4>Is this your first pet?</h4>
         <br />
         <form>
-          <input type="checkbox" />
+          <input type="checkbox" onChange={handlePet}/>
         </form>
-        <h5></h5>
+        <h5>{checked ? "Yes" : "No"}</h5>
       </section>
     );
-  }
 }
 
 export default FirstPet;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://vet-lab-8-4.herokuapp.com/api/pets";
 
-class PetList extends React.Component {
-  render() {
+const PetList = () => {
+  const [allPets, setAllPets] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => setAllPets(data))
+  }, [])
+
+  console.log(allPets)
+
     return (
       <section className="pet-list">
         <h4>All Pets</h4>
-        <article></article>
+        <article>
+          {allPets.map(pet => {
+            console.log(pet)
+          return <Pet name={pet.name} kind={pet.kind} breed={pet.breed}/>
+          })}
+        </article>
       </section>
     );
-  }
 }
 
 export default PetList;
