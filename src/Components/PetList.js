6f77a0800923fc6e6129154b5d7b2668ca192412
T.K <nique@Niques-MacBook-Air.local>
2022-06-29 13:22:32 -0400

    fixing petlist.js

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,14 +1,28 @@
-import React from "react";
-// import Pet from "./Pet";
+import React, { useEffect, useState } from "react";
+import Pet from "./Pet";
 import "./PetList.css";
 
-// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://vet-lab-8-4.herokuapp.com/api/pets";
+
 
 export default function PetList() {
+
+  const [pets, setPets] = useState([]);
+
+useEffect(() => {
+  fetch(BASE_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      setPets(data)
+    })
+})
+
   return (
     <section className="pet-list">
       <h4>All Pets</h4>
-      <article></article>
+      <article>
+        <Pet pets={ pets }/>
+      </article>
     </section>
   );
 }
