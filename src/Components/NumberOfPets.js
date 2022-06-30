3b11c288180d885c8dc01bcd29847aed86d01f4f
Dannell <dannellmorales@pursuit.org>
2022-06-29 21:03:18 -0400

    React w/Hooks

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
-  }
+function NumberOfPets() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <section className="num-of-pets">
+    <h4>Number of Pets</h4>
+    <h5>numOfPets: {count}</h5>
+    <div>
+      <button id="less" onClick={()=> setCount(count +1)}>-</button>
+      <button id="more" onClick={()=> setCount(count -1)}>+</button>
+    </div>
+  </section>
+  )
 }
 
-export default NumberOfPets;
+export default NumberOfPets
+
+
+
+
+
+
+// class NumberOfPets extends React.Component {
+//   render() {
+//     return (
+//       <section className="num-of-pets">
+//         <h4>Number of Pets</h4>
+//         <h5>numOfPets</h5>
+//         <div>
+//           <button id="less">-</button>
+//           <button id="more">+</button>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+// export default NumberOfPets;
