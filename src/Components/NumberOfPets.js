d49ffe1f9217199a8a4ac883a2e8a5edc5a61544
Andres Varon <anvaron@gmail.com>
2022-07-01 09:38:10 -0500

    NumOfPets: - [Work with numbers, work with multiple functions] - tested

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
-  }
+const NumberOfPets = () => {
+  const [ numOfPets, setNumOfpets ] = useState(1)
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button id="less" onClick={() => setNumOfpets(numOfPets - 1)}>-</button>
+        <button id="more" onClick={() => setNumOfpets(numOfPets + 1)}>+</button>
+      </div>
+    </section>
+  );
 }
 
 export default NumberOfPets;
