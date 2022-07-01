47e60a31d1f7b7e74c22000aee101e0fe708a41c
Jonathan Scheiber <jon@scheiber.me>
2022-07-01 17:36:42 -0400

    update AnimalTypes.js to use hooks

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,23 +1,50 @@
-import React from "react";
+import { useState } from "react";
 import "./AnimalTypes.css";
 
 const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
-class AnimalTypes extends React.Component {
-  render() {
-    return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol></ol>
-      </section>
-    );
-  }
-}
+const AnimalTypes = () => {
+  const [animalList, setAnimalList] = useState(animalTypes);
+  const [text, setText] = useState("");
+
+  const handleRemove = (animal) => {
+    const ind = animalList.indexOf(animal);
+    const copyList = [...animalList];
+    copyList.splice(ind, 1);
+    setAnimalList(copyList);
+  };
+  const handleChange = (e) => {
+    setText(e.target.value.toLocaleLowerCase());
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const ind = animalList.indexOf(text);
+    if (ind === -1) setAnimalList([...animalList, text]);
+    setText("");
+  };
+  return (
+    <section className="animal-types">
+      <h4>Animal Types</h4>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="animal-type">
+          <input
+            type="text"
+            id="animal-type"
+            value={text}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <ol>
+        {animalList.map((animal, i) => (
+          <li key={i}>
+            {animal} <button onClick={() => handleRemove(animal)}>-</button>
+          </li>
+        ))}
+      </ol>
+    </section>
+  );
+};
 
 export default AnimalTypes;
