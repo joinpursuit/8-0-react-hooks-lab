9fe909ef5bff264a563247811cfa4e3582337276
Lortiz528 <lortiztech@gmail.com>
2022-07-01 12:09:30 -0400

    petList works

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './FirstPet.css';
-import {useState} from 'react'
+import { useState } from 'react';
 
-const FirstPet = () =>{
-  const [checked, setChecked] = useState(false)
+const FirstPet = () => {
+  const [checked, setChecked] = useState(false);
   return (
     <section className="first-pet">
       <h4>Is this your first pet?</h4>
       <br />
       <form>
-        <input type="checkbox" checked ={checked} onChange={(event) => setChecked(event.target.checked)} />
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={(event) => setChecked(event.target.checked)}
+        />
       </form>
-      <h5>{checked ? 'Yes' : 'No' }</h5>
+      <h5>{checked ? 'Yes' : 'No'}</h5>
     </section>
   );
-}
+};
 
 export default FirstPet;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -6,11 +6,22 @@ import { useEffect, useState } from 'react';
 //const BASE_URL = 'http://localhost:5000/api';
 
 function PetList() {
+  const [petInfo, setPetInfo] = useState([]);
+
+  useEffect(() => {
+    fetch('https://serene-tundra-77911.herokuapp.com/api/pets')
+      .then((res) => res.json())
+      .then((data) => setPetInfo(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <section className="pet-list">
       <h4>All Pets</h4>
       <article>
-        <Pet />
+        {petInfo.map((pet) => {
+          return <Pet name={pet.name} kind={pet.kind} breed={pet.breed} />;
+        })}
       </article>
     </section>
   );
