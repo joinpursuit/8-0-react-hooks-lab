43d9feac585ec9c86c4d8de8b1ba4f7641beb6c0
named.josie <named.josie@gmail.com>
2022-06-30 22:42:33 -0400

    done

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,23 +1,69 @@
-import React from "react";
-import "./AnimalTypes.css";
+import { useState } from 'react';
+import './AnimalTypes.css';
 
-const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
+export default function AnimalTypes() {
+  const animalTypes = [
+    'dog',
+    'cat',
+    'ferret',
+    'bird',
+    'fish',
+    'snake',
+    'lizard',
+  ];
 
-class AnimalTypes extends React.Component {
-  render() {
-    return (
-      <section className={"animal-types"}>
-        <h4>Animal Types</h4>
-        <form>
-          <label htmlFor="type">
-            <input type="text" id="animal-type" />
-          </label>
-          <input type="submit" />
-        </form>
-        <ol></ol>
-      </section>
-    );
-  }
+  const [inputAnimal, setInputAnimal] = useState('');
+
+  const [animalsList, setAnimalsList] = useState(animalTypes);
+
+  const handleChange = (event) => {
+    setInputAnimal(event.target.value.toLowerCase());
+  };
+
+  const addAnimal = (userInput) => {
+    const newAnimal = [...animalsList, userInput];
+    setAnimalsList(newAnimal);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!animalsList.includes(inputAnimal)) addAnimal(inputAnimal);
+
+    setInputAnimal('');
+  };
+
+  const handleClick = (index) => {
+    let newList = [...animalsList];
+
+    newList.splice(index, 1);
+
+    setAnimalsList(newList);
+  };
+  return (
+    <section className={'animal-types'}>
+      <h4>Animal Types</h4>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='type'>
+          <input
+            type='text'
+            id='animal-type'
+            value={inputAnimal}
+            onChange={handleChange}
+          />
+        </label>
+        <input type='submit' />
+      </form>
+      <ol>
+        {animalsList.map((animal, index) => {
+          return (
+            <li key={index}>
+              {animal}
+              <button onClick={() => handleClick(index)}>Remove</button>
+            </li>
+          );
+        })}
+      </ol>
+    </section>
+  );
 }
-
-export default AnimalTypes;
