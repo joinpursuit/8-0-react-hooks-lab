0b5490a466f2a2900ffbb01041698d56caddb5d0
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-07-07 16:55:18 -0400

    Changes

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -1,19 +1,57 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./AnimalTypes.css";
 
-const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
-
 const AnimalTypes = () => {
+  const [newAnimal, setAnimal] = useState("");
+  const [animalTypes, newTypes] = useState([
+    "dog",
+    "cat",
+    "ferret",
+    "bird",
+    "fish",
+    "snake",
+    "lizard"
+  ]);
+
+  const handleAnimal = (e) => {
+    setAnimal(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    newTypes((prevAnimals) => {
+    });
+  };
+
+  const handleDelete = (e) => {
+    animalTypes.splice(animalTypes.indexOf(e.target.key), 1);
+  };
+
   return (
     <section className={"animal-types"}>
       <h4>Animal Types</h4>
       <form>
         <label htmlFor="type">
-          <input type="text" id="animal-type" />
+          <input
+            type="text"
+            id="animal-type"
+            value={newAnimal}
+            onChange={handleAnimal}
+          />
         </label>
-        <input type="submit" />
+        <input type="submit" onSubmit={handleSubmit} />
       </form>
-      <ol></ol>
+      <ol>
+        {animalTypes.map((animal) => {
+          return (
+            <li key={animal}>
+              {animal}
+              <button onClick={handleDelete} />
+            </li>
+          )
+        })}
+      </ol>
     </section>
   );
 };
--- src/Components/NewClient.js
+++ src/Components/NewClient.js
@@ -1,26 +1,56 @@
 import React from "react";
+import { useState } from "react";
 import "./NewClient.css";
 
 const NewClient = () => {
+  const [firstName, setFirst] = useState("");
+  const [lastName, setLast] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const fieldChange = (e) => {
+    if (e.target.id === "firstName") {
+      setFirst(e.target.value);
+    }
+    if (e.target.id === "lastName") {
+      setLast(e.target.value);
+    }
+    if (e.target.id === "phone") {
+      setPhone(e.target.value);
+    }
+    if (e.target.id === "email") {
+      setEmail(e.target.value);
+    }
+  };
   return (
     <section className="new-client">
       <h4>New Client Info</h4>
       <div>
         <form>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" id="firstName" />
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={fieldChange}
+          />
           <label htmlFor="lastName">Last Name</label>
-          <input type="text" id="lastName" />
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={fieldChange}
+          />
           <label htmlFor="phone">Telephone</label>
-          <input type="tel" id="phone" />
+          <input type="tel" id="phone" value={phone} onChange={fieldChange} />
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" value={email} onChange={fieldChange} />
         </form>
         <article>
-          <h5>firstName</h5>
-          <h5>lastName</h5>
-          <h5>phone</h5>
-          <h5>email</h5>
+          <h5>{firstName}</h5>
+          <h5>{lastName}</h5>
+          <h5>{phone}</h5>
+          <h5>{email}</h5>
         </article>
       </div>
     </section>
