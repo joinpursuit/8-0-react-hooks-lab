3d6ca2331f9bbc47d7bacc668cab7e51d20059c9
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-11-08 02:04:06 -0500

    done

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,24 @@
-import React from "react";
+import { React, useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
-  }
-}
+export default function NumberOfPets() {
+  const [numOfPets, setNumOfPets] = useState(0);
 
-export default NumberOfPets;
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button
+          id="less"
+          onClick={() => (numOfPets > 0 ? setNumOfPets(numOfPets - 1) : null)}
+        >
+          -
+        </button>
+        <button id="more" onClick={() => setNumOfPets(numOfPets + 1)}>
+          +
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,18 +1,29 @@
-import React from "react";
+import { useState, useEffect, React } from "react";
 import Pet from "./Pet";
 import "./PetList.css";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://vet-resource-api-9-2.herokuapp.com/api/pets";
 
-class PetList extends React.Component {
-  render() {
-    return (
-      <section className="pet-list">
-        <h4>All Pets</h4>
-        <article></article>
-      </section>
-    );
-  }
+export default function PetList() {
+  const [pets, setPets] = useState([]);
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((data) => setPets(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <section className="pet-list">
+      <h4>All Pets</h4>
+      {pets.map((pet) => {
+        return (
+          <article>
+            {" "}
+            <Pet name={pet.name} breed={pet.breed} kind={pet.kind} />{" "}
+          </article>
+        );
+      })}
+    </section>
+  );
 }
-
-export default PetList;
